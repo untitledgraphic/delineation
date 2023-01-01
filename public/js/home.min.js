91f3a9b86f49eddd9da40517525b43961f5e2bbc
Craig Cooper <craigcooper@Liberator.local>
2023-01-01 08:57:18 +0000

    begin reorganising
    strip out crappy javascript
    split up some css etc

--- public/js/home.min.js
+++ public/js/home.min.js
@@ -3,124 +3,32 @@
 var app = (function ($) {
     'use strict';
 
-    // private alias to settings
-    //var s;
-
     return {
         settings: function () {
             this.$window        = $(window);
         },
 
-        init: function () {
-            //var s = new this.settings();
-            
-            this.bindUIActions();       // UI Actions (click, focus etc)
-            this.nav();                 // Main navigation
-//            this.features();            // do stuff on scroll (Scrollify)
-            this.form();                // form type stuff
-            this.lazy();                // lazyload config
+        init: function () {            
             this.parallax();            // parallaxy images
-            this.scrollon();            // scroll down a bit
         },
+             
+        // to be converted to vanilla JS and moved to a view file
 
-        bindUIActions: function() {
+//         form: function() {
             
-            ////////// LOGO DEBOSS ///////////
-            setTimeout(function(){
-                $('body').addClass('loaded');
-            }, 2000);
-        },
-        
-        nav: function() {
-            var nav = '#mainNav';
-            var navon = 'mainNav--active';
-            
-            $('#navTrigger').click(function(e) {
-                e.preventDefault();
-                $(nav).addClass(navon);
-            });
-            $('.mainNav__close').click(function(e) {
-                e.preventDefault();
-                $(nav).removeClass(navon);
-            });
-            $('.mainNav__link').click(function() {
-                $(nav).removeClass(navon);
-            });
-            
-            $('#works').click(function() {
-                
-                var first = '[data-slide="1"]';
-                var active = 'active';
-                
-                if ($(first).hasClass(active)) {
-                    //do nothing
-                }
-                else if ($('.slide').not(first).hasClass(active)) { 
-                    $('.slide').not(first).removeClass(active);
-                    $(first).addClass(active);
-                }
-                else {
-                    $(first).addClass(active);
-                }
-            });
-        },
-        
-        scrollon: function() {
-            
-            var scrollTrigger = function() {
-                $('#helper').on('click', function(e) {
-                    e.preventDefault(); 
-
-                    var scrollPos = $(document).scrollTop();
-
-                    $('html, body').animate({ 
-                        scrollTop: $(window).height() + scrollPos  //Get the document height
-                    }, 'slow'); //Animates the scroll
-                });
-            };
-        
-            $(window).on('load resize', function() {
-                
-                if (window.location.href !== 'http://localhost:3000/') {
-                    scrollTrigger();
-                }
-                else if ($(this).width() < 768) {
-                    scrollTrigger();
-                }
-            });
-        },
-        
-        form: function() {
-            
-            $('.form__txt').blur(function() {
-                var tmpval = $(this).val();
-                if(tmpval === '') {
-//                    $(this).addClass('empty');
-                    $(this).removeClass('filled');
-                } else {
-                    $(this).addClass('filled');
-//                    $(this).removeClass('empty');
-                }
-            });
-        },
-        
-        lazy: function() {
-
-            $('.lazy').Lazy({
-                // your configuration goes here
-                scrollDirection: 'vertical',
-                effect: 'fadeIn',
-                effectTime: 1000,
-                visibleOnly: true,
-                onError: function(element) {
-                    console.log('error loading ' + element.data('src'));
-                },
-                afterLoad: function(e) {
-                    $(this).addClass('loadeded');
-                    console.log('loaded' + e.data('src'));
-                }
-            });
-        },
+//             $('.form__txt').blur(function() {
+//                 var tmpval = $(this).val();
+//                 if(tmpval === '') {
+// //                    $(this).addClass('empty');
+//                     $(this).removeClass('filled');
+//                 } else {
+//                     $(this).addClass('filled');
+// //                    $(this).removeClass('empty');
+//                 }
+//             });
+//         },
+          
+        // to convert to vanilla js and moved to home 
         
         parallax: function() {
             
